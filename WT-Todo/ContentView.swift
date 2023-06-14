bfebb6e00290b95c51a591f79390e8067f8e2ee7
Lonnie Gerol <lonnie@lonniegerol.com>
2023-06-14 19:55:52 -0400

    Initial Commit

---
+++ WT-Todo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WT-Todo
+//
+//  Created by Lonnie Gerol on 6/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WT-Todo/WT_TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WT_TodoApp.swift
+//  WT-Todo
+//
+//  Created by Lonnie Gerol on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct WT_TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
